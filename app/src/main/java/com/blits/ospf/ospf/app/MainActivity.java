69b4e6033f9cfe6242907aa673faad58e027a814
andiyulistyo <andiyulistyo@gmail.com>
2014-03-20 22:52:17 +0700

    add class

--- app/src/main/java/com/blits/ospf/ospf/app/MainActivity.java
+++ app/src/main/java/com/blits/ospf/ospf/app/MainActivity.java
@@ -1,51 +1,64 @@
 package com.blits.ospf.ospf.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.devspark.appmsg.AppMsg;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
-    private Button button;
+    private EditText editTextInput;
+    private Button buttonNext;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.buttonSelanjutnyaInputRouter);
-        button.setOnClickListener(this);
+        editTextInput = (EditText) findViewById(R.id.editTextInputRouter);
+        buttonNext = (Button) findViewById(R.id.buttonSelanjutnyaInputRouter);
+        buttonNext.setEnabled(false);
 
+        editTextInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().trim().isEmpty())  {
+                    buttonNext.setEnabled(true);
+                } else  {
+                    buttonNext.setEnabled(false);
+                }
+            }
+        });
+
+        buttonNext.setOnClickListener(this);
     }
 
 
+    @SuppressWarnings("ConstantConditions")
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    public void onClick(View view) {
+        // ambil nilai dari inputan  user
+        int router = Integer.valueOf(editTextInput.getText().toString());
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        AppMsg.makeText(this, "Masih Kosong TU", AppMsg.STYLE_ALERT).show();
+        // pass ke class selanjutnya
+        Intent intent = new Intent(getApplication(), NamaRouter.class);
+        intent.putExtra("router", router);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/blits/ospf/ospf/app/adapter/NamaRouterAdapter.java
@@ -0,0 +1,85 @@
+package com.blits.ospf.ospf.app.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+
+import com.blits.ospf.ospf.app.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class NamaRouterAdapter extends BaseAdapter {
+
+    private List<String> nama = new ArrayList<>();
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    public HashMap<Integer, String> hashMap = new HashMap<>();
+
+
+    public NamaRouterAdapter(List<String> nama, Context mcontext) {
+        this.nama = nama;
+        this.context = mcontext;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return nama.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return nama.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup viewGroup) {
+        final ViewHold viewHold;
+
+        if (view == null) {
+            viewHold = new ViewHold();
+            view = layoutInflater.inflate(R.layout.item_input_nama_router, viewGroup, false);
+
+            viewHold.editTextNama = (EditText) (view != null ? view.findViewById(R.id.editTextNamaRouter) : null);
+
+            if (view != null) {
+                view.setTag(viewHold);
+            }
+
+        } else viewHold = (ViewHold) view.getTag();
+
+
+        if (hashMap.isEmpty()) {
+            viewHold.editTextNama.setText("");
+        } viewHold.editTextNama.setText(hashMap.get(position));
+
+        viewHold.editTextNama.setId(position);
+
+        viewHold.editTextNama.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @SuppressWarnings("ConstantConditions")
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    hashMap.put(position, viewHold.editTextNama.getText().toString());
+                }
+            }
+        });
+
+        return view;
+    }
+
+    class ViewHold {
+        private EditText editTextNama;
+    }
+}
