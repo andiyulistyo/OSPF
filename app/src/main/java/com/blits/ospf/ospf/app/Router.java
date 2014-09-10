66cf11fc716d620e80fe697a010dbb24d9b930e0
souttab <andiyulistyo@gmail.com>
2014-09-10 22:46:06 +0700

    update UI router input

---
+++ app/src/main/java/com/blits/ospf/ospf/app/Router.java
@@ -0,0 +1,89 @@
+package com.blits.ospf.ospf.app;
+
+/**
+ * Created by andiyulistyo on 10/09/14.
+ */
+public class Router {
+
+    private String namaRouter;
+    private String portSE0;
+    private String portSE1;
+    private String portFA0;
+    private String portFA1;
+    private String ipSE0;
+    private String ipSE1;
+    private String ipFA0;
+    private String ipFA1;
+
+    public String getNamaRouter() {
+        return namaRouter;
+    }
+
+    public void setNamaRouter(String namaRouter) {
+        this.namaRouter = namaRouter;
+    }
+
+    public String getPortSE0() {
+        return portSE0;
+    }
+
+    public void setPortSE0(String portSE0) {
+        this.portSE0 = portSE0;
+    }
+
+    public String getPortSE1() {
+        return portSE1;
+    }
+
+    public void setPortSE1(String portSE1) {
+        this.portSE1 = portSE1;
+    }
+
+    public String getPortFA0() {
+        return portFA0;
+    }
+
+    public void setPortFA0(String portFA0) {
+        this.portFA0 = portFA0;
+    }
+
+    public String getPortFA1() {
+        return portFA1;
+    }
+
+    public void setPortFA1(String portFA1) {
+        this.portFA1 = portFA1;
+    }
+
+    public String getIpSE0() {
+        return ipSE0;
+    }
+
+    public void setIpSE0(String ipSE0) {
+        this.ipSE0 = ipSE0;
+    }
+
+    public String getIpSE1() {
+        return ipSE1;
+    }
+
+    public void setIpSE1(String ipSE1) {
+        this.ipSE1 = ipSE1;
+    }
+
+    public String getIpFA0() {
+        return ipFA0;
+    }
+
+    public void setIpFA0(String ipFA0) {
+        this.ipFA0 = ipFA0;
+    }
+
+    public String getIpFA1() {
+        return ipFA1;
+    }
+
+    public void setIpFA1(String ipFA1) {
+        this.ipFA1 = ipFA1;
+    }
+}
