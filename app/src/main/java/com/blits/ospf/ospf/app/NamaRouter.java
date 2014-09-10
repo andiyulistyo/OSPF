b8274dbee46598f6a66b9884807af775388cab4c
souttab <andiyulistyo@gmail.com>
2014-09-10 22:35:19 +0700

    sebelum di pake git flow

--- app/src/main/java/com/blits/ospf/ospf/app/NamaRouter.java
+++ app/src/main/java/com/blits/ospf/ospf/app/NamaRouter.java
@@ -3,8 +3,7 @@ package com.blits.ospf.ospf.app;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
-import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -33,7 +32,7 @@ public class NamaRouter extends ListActivity implements View.OnClickListener {
         List<String> nama = new ArrayList<String>();
 
         for (int i = 0; i < router; i++) {
-            nama.add("Nama Router ke : "+i);
+            nama.add("Nama Router ke : " + i);
         }
 
         // set data ke adapter
@@ -64,32 +63,4 @@ public class NamaRouter extends ListActivity implements View.OnClickListener {
         intent.putStringArrayListExtra("nama", arrayList);
         startActivity(intent);
     }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater menuInflater = getMenuInflater();
-//        menuInflater.inflate(R.menu.main, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == R.id.next) {
-//            ArrayList<String> arrayList = new ArrayList<>();
-//
-//            // get value dari inputan di listview
-//            for (int i = 0; i < routerAdapter.getCount(); i++) {
-//                // ambil data dari adapter
-//                arrayList.add(routerAdapter.hashMap.get(i));
-//            }
-//
-//            // panggil class selanjutnya dan kirim value nama yang telah
-//            // diinputkan
-//            Intent intent = new Intent(NamaRouter.this, InputDetailRouter.class);
-//            intent.putStringArrayListExtra("nama", arrayList);
-//            startActivity(intent);
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
